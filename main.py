894119b71fd5559404988ee9bf61777bd23deef6
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-07-31 07:41:17 +0300

    commit all

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-from fastapi import FastAPI
+
+
+from typing import List
+
+import datetime
+from fastapi import FastAPI, Request, HTTPException
+from pydantic.v1 import BaseModel
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/ping")
+async def ping():
+    return {"pong": "pong"}
+
+@app.get("/home")
+async def home():
+    with open("home.html", "r",encoding="utf-8") as file:
+        html_content=file.read()
+        return HTMLResponse(content=html_content)
+
+class Books(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_date:datetime.datetime
+
+book_db :List[Books] =[]
+@app.post("/posts")
+async def posts(new_book:List[Books]):
+    book_db.extend(new_book)
+    return book_db
+
+@app.get("/posts")
+async def get_posts():
+    return book_db
+
+@app.put("/posts")
+async def update_posts(book:Books):
+    for i,existing_book in enumerate(book_db):
+        if book.title == existing_book.title:
+            book_db[i] = book
+        book_db.append(book)
+        return book_db
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
