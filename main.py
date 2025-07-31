c63d38a176948b84aa3d7db974b5cb19c294631b
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-07-31 08:40:00 +0300

    commit 1.2

--- main.py
+++ main.py
@@ -20,11 +20,6 @@ async def home():
         html_content=file.read()
         return HTMLResponse(content=html_content)
 
-@app.get("/{full_path:path}")
-def error():
-   with open("404.html", "r",encoding="utf-8") as file:
-       html_content=file.read()
-       return HTMLResponse(content=html_content)
 
 
 class Books(BaseModel):
@@ -34,16 +29,16 @@ class Books(BaseModel):
     creation_date:str
 
 book_db :List[Books] =[]
-@app.post("/posts")
+@app.post("/posts",status_code=201,response_model=List[Books])
 async def posts(new_book:List[Books]):
     book_db.extend(new_book)
     return book_db
 
-@app.get("/posts")
+@app.get("/posts",response_model=List[Books],status_code=200)
 async def get_posts():
     return book_db
 
-@app.put("/posts")
+@app.put("/posts",status_code=202,response_model=List[Books])
 async def update_posts(book:Books):
     for i,existing_book in enumerate(book_db):
         if book.title == existing_book.title:
@@ -60,3 +55,9 @@ async def get_auth(request:Request):
     if auth != "123456":
         raise HTTPException(status_code=401,detail="Authorization header invalid")
     return Response("pong")
+
+@app.get("/{full_path:path}",status_code=404)
+def error():
+    with open("404.html", "r",encoding="utf-8") as file:
+        html_content=file.read()
+        return HTMLResponse(content=html_content)
